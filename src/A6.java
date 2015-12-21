7de323a250c8181a6a48769b343f4f5389211b55
ray <ray@rrr>
2015-12-21 14:14:53 +0800

    sixth code model

--- src/A6.java
+++ src/A6.java
@@ -1,21 +1,48 @@
-//user name D0280184
+/*
+ * user name D0280184
+ */
+import java.util.ArrayList;
 public class A6 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 //dfafdasfd
+		Component test = new TestPaper();
+		if (test instanceof TestPaper) {
+			test.addQuestion(new Question("1 + 1 = ?"));
+		}
+		test.doQuestion();
 	}
-
 }
-class Question {
-	   String description;
-	   public Question(String desc) {
-	      this.description = desc;
-	   }
-	   public void print() {
-	      System.out.println(description);
-	   }      
+abstract class Component {
+	abstract void doQuestion();
+	abstract void addQuestion(Question question);
+}
+
+class Question extends Component {
+	String description;
+	public Question(String desc) {
+		this.description = desc;
+	}
+	public void print() {
+		System.out.println(this.description);
+	}
+	@Override
+	void doQuestion() {
+		print();
+	}
+	@Override
+	void addQuestion(Question question) {
+	}
+}
+
+class TestPaper extends Component {
+	ArrayList<Component> list;
+	public TestPaper() {
+		list = new ArrayList<>();
+	}
+	public void addQuestion(Question q) {
+		list.add(q);
+	}
+	@Override
+	void doQuestion() {
+		list.forEach(Component::doQuestion);
+	}
 }
